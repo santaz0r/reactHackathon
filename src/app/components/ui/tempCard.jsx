91f0fd6ab880941bf78afb812cd56023df5438eb
VictorM <neuron96@mail.ru>
2022-03-12 16:50:20 +0400

    another fixes

--- src/app/components/ui/tempCard.jsx
+++ src/app/components/ui/tempCard.jsx
@@ -1,26 +1,7 @@
 import React from "react";
-import team from "../../team.js";
 
 const TempCard = () => {
-    return (
-        <ul className="d-flex justify-content-center">
-            {team.users.map((user) => (
-                <div key={user.id}>
-                    <img
-                        src="https://miro.medium.com/max/512/1*jA5lTgPRbyimsFNod7SlFQ.png"
-                        alt="image"
-                    ></img>
-                    <li>
-                        <h3>{user.name}</h3>
-                    </li>
-                    <button className="btn btn-primary m-2">
-                        Открыть карточку
-                    </button>
-                    <h4>Немного об участнике: {user.description}</h4>
-                </div>
-            ))}
-        </ul>
-    );
+    return <h1>тут страница участника</h1>;
 };
 
 export default TempCard;
