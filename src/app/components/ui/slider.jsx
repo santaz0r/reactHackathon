a504ddd5c46b20308aff5b336627f7895ce10e1e
Dan Tkachuk <daniil.tkachuk@gmail.com>
2022-03-12 19:10:45 +0300

    Add container to slider

--- src/app/components/ui/slider.jsx
+++ src/app/components/ui/slider.jsx
@@ -4,23 +4,25 @@ import PropTypes from "prop-types";
 
 const Slider = ({ data }) => {
     return (
-        <Carousel>
-            {data.map((slide, i) => {
-                return (
-                    <Carousel.Item key={`slide-${i}`}>
-                        <img
-                            className="d-block w-100"
-                            src={slide.imgSrc}
-                            alt="First slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>{slide.title}</h3>
-                            <p>{slide.descr}</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                );
-            })}
-        </Carousel>
+        <div className="container">
+            <Carousel>
+                {data.map((slide, i) => {
+                    return (
+                        <Carousel.Item key={`slide-${i}`}>
+                            <img
+                                className="d-block w-100"
+                                src={slide.imgSrc}
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>{slide.title}</h3>
+                                <p>{slide.descr}</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    );
+                })}
+            </Carousel>
+        </div>
     );
 };
 Slider.defaultProps = {
